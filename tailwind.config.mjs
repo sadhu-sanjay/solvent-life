ebfa7df00785361d5f1e4e472628f61a51fbe84b
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-03-25 01:45:06 +0530

    updated the app

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: 'var(--aw-color-primary)',
+				secondary: 'var(--aw-color-secondary)',
+				accent: 'var(--aw-color-accent)',
+				default: 'var(--aw-color-text-default)',
+				muted: 'var(--aw-color-text-muted)',
+			}
+		},
 	},
 	plugins: [],
 }
